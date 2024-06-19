32ef5aab04ab8e6f862b03134a14c2528f043143
Valentyn Sobol <valiksobol14@gmail.com>
2024-06-19 20:54:53 +0300

    make build

---
+++ index.db966db2.js
@@ -0,0 +1,2 @@
+var e,t,c=document.querySelector(".wall"),l=c.clientWidth/2,o=c.clientHeight/2,i=document.querySelector(".spider");e=i.offsetWidth,t=i.offsetHeight,i.style.left="".concat(l-e/2,"px"),i.style.top="".concat(o-t/2,"px");
+//# sourceMappingURL=index.db966db2.js.map
